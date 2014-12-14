6bcfc5ea2f45abfb5e9bec772fb61e4609f2b523
AdrianM <adrianm1@outlook.com>
2014-12-14 12:57:53 -0700

    Se agrego la clase encargada de analisar el texto y compararlo

---
+++ src/feelings/analyser/models/AnalyzerText.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package feelings.analyser.models;
+
+/**
+ *
+ * @author AdrianM
+ */
+public class AnalyzerText {
+    private String work = "";
+    
+}
